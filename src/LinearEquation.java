141d5b586a73867226fc95ac4fe9150e6d1d0f2b
BT_1N3_20 <slavicservant@gmail.com>
2023-11-03 09:30:03 -0400

    duajsdfaj

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,108 @@
-package PACKAGE_NAME;public class LinearEquation {
+class LinearEquation {
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
+    private String cord1;
+
+    private String cord2;
+    private double yInter;
+
+    private double slope;
+
+    private int denom;
+    private int numer;
+
+    private String slopeDefiner = "normal";
+
+    public LinearEquation(String cord1, String cord2) {
+        this.cord1 = cord1;
+        this.cord2 = cord2;
+
+        String temp1 = cord1.substring(1, cord1.indexOf(','));
+        String temp2 = cord1.substring(cord1.indexOf(',') + 1, cord1.indexOf(')'));
+
+        String temp3 = cord2.substring(1, cord2.indexOf(','));
+        String temp4 = cord2.substring(cord2.indexOf(',') + 1, cord2.indexOf(')'));
+
+        x1 = Integer.parseInt(temp1);
+        y1 = Integer.parseInt(temp2);
+        x2 = Integer.parseInt(temp3);
+        y2 = Integer.parseInt(temp4);
+
+        denom = (x2 - x1);
+        numer = (y2 - y1);
+    }
+
+    public double getSlope() {
+        slope = (double) (y2 - y1) / (x2 - x1);
+        return slope;
+    }
+
+    public double getYInter() {
+        yInter = y1 - slope * x1;
+        return yInter;
+    }
+
+    public double distanceBetween() {
+        double aSquared = denom * denom;
+        double bSquared = numer * numer;
+        double csquared = aSquared + bSquared;
+        return (int) ((Math.sqrt(csquared) * 100) + 0.5) / 100.0;
+    }
+
+    public String getM() {
+        return numer + "/" + denom;
+    }
+
+    public String theEquation(double x) {
+        if (x1 != x2 && y1 != y2){
+            double y = slope * x + yInter;
+            return "(" + x + "," + y + ")";
+        }
+        else if (x1 == x2 && y1 != y2){
+            return "(-inf, -inf) all real values";
+        }
+        else if (x1 != x2 && y1 == y2){
+            return "" + y1 + "";
+        }
+        else{
+            return "only one point given, no line found.";
+        }
+
+    }
+    public String lineInfo() {
+        if (x1 != x2 && y1 != y2){
+            return "The two points are: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")\n"
+                    + "The equation of the line between these points is: y = " + getM() + "x + " + getYInter() + "\n"
+                    + "The slope of this line is: " + getSlope() + "\n"
+                    + "The y-intercept of this line is: " + getYInter() + "\n"
+                    + "The distance between these points is " + distanceBetween();
+        }
+        else{
+
+            if (x1 != x2 && y1 == y2){
+                return "The two points are: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")\n"
+                        + "The equation of the line between these points is: y = " + y2 + "\n"
+                        + "The slope of this line is: 0 a horizontal line has no slope \n"
+                        + "The y-intercept of this line is: " + getYInter() + "\n"
+                        + "The distance between these points is " + distanceBetween();
+            }
+            if (x1 == x2 && y1 != y2){
+                return "The two points are: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")\n"
+                        + "The equation of the line between these points is: y = " + x2 + "\n"
+                        + "The slope of this line is: " + getSlope() + "\n"
+                        + "The y-intercept of this line is: A vertical line has no y-intercept \n"
+                        + "The distance between these points is " + distanceBetween();
+            }
+            if (x1 == x2 && y1 == y2){
+                return "The two points are: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")\n"
+                        + "the points are the same thus there is no slope or y intercept";
+            }
+            else{
+                return "null";
+            }
+        }
+    }
 }
--- src/LinearEquationLogic.java
+++ src/LinearEquationLogic.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class LinearEquationLogic {
+import java.util.Scanner;
+class LinearEquationLogic {
+    private Scanner scan;
+
+    public LinearEquationLogic() {
+        scan = new Scanner(System.in);
+    }
+    public void start() {
+        boolean repeat = true;
+        while (repeat) {
+            System.out.println("Welcome to the linear equation calculator! \n");
+            System.out.println("Enter the first coordinate: ");
+            String cord1 = scan.nextLine();
+            System.out.println("Enter the second coordinate: ");
+            String cord2 = scan.nextLine();
+
+            LinearEquation line1 = new LinearEquation(cord1, cord2);
+
+            System.out.println(line1.lineInfo());
+
+            System.out.println("Enter the x value to find the corresponding y: ");
+            double x = scan.nextDouble();
+            System.out.println("Coordinate point on the line: " + line1.theEquation(x));
+
+            System.out.println("Do you want to enter more coordinates? (yes/no): ");
+            String response = scan.next();
+            repeat = response.equals("yes");
+            scan.nextLine();
+        }
+        scan.close();
+    }
 }
